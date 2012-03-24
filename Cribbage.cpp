a49073ed6154292a9940ac7513429147bdbb4e12
Kyle Robinson <kylerob89@gmail.com>
2012-03-24 00:02:49 -0400

    Moved stuff for no reason

--- Cribbage.cpp
+++ Cribbage.cpp
@@ -1,5 +1,15 @@
 #include "Cribbage.h"
-
+/*
+ * -Need number of human/AI players
+ * -Randomly choose first player and make sure turns are taken clockwise
+ * -Deal cards based on that number
+ * -Everyone throws into the kitty and possibly a card from the deck is put in the kitty
+ * -Cut the deck
+ * -Play
+ * -Reveal hand
+ * -Calculate own hand and kitty
+ * -Allow for human players to calculate their own score and AI can correct
+ */
 Cribbage::Cribbage()
 {    
 }
--- game.cpp
+++ game.cpp
@@ -8,14 +8,11 @@ int main()
     deck.init();
     deck.shuffle();
     deck.printDeck();
-    cout<<"Number of cards left in the deck: "<<deck.numCardsLeft()<<endl;
     deck.deal(hand);
     deck.deal(hand);
-    cout<<"Number of cards left after deal() called: "<<deck.numCardsLeft()<<endl;
 
     for(vector<card>::iterator it = hand.begin(); it!=hand.end(); it++)
     {
         cout<<endl<<endl<<it->val<<it->suit<<endl;
     }
-    cout<<"lakjsdklajsdlkj"<<endl;
 }
